18e58fdaf925eaeca394b5d39b421c7cfb61e91f
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-04-19 11:23:40 +0800

    implement clearing logs

--- src/commands/ClearLogs.php
+++ src/commands/ClearLogs.php
@@ -11,7 +11,15 @@
 
 		public function handle($className = ''): void
 		{
-			$this->info('Performing clear application logs...');
-			$this->success('Successfully cleared the application logs.');
+			$this->info('Clearing application logs...');
+
+			$path = dirname('./') . '/logs';
+
+			if (is_dir($path)) {
+				rmdir($path);
+				$this->success('Application logs cleared successfully.');
+			} else {
+				$this->warn('No log directory found to clear.');
+			}
 		}
 	}

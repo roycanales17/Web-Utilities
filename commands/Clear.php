3d3f4dd9fc6e2bfc7f1dc0eb4e57826fe924f67e
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-04-22 20:46:48 +0800

    add clear function

---
+++ commands/Clear.php
@@ -0,0 +1,23 @@
+<?php
+
+	namespace Commands;
+
+	use App\Console\Command;
+	use App\Console\Terminal;
+
+	class Clear extends Command
+	{
+		protected string $signature = 'clear';
+		protected string $description = 'Clear the terminal screen';
+
+		public function handle(): void
+		{
+			if (DIRECTORY_SEPARATOR === '\\') {
+				system('cls');
+			} else {
+				system('clear');
+			}
+
+			Terminal::handle('list');
+		}
+	}

64182faf0e2076a57ea23e6d796ebbe3d9224f3e
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-05-07 23:38:03 +0800

    improve creating a component

--- commands/Component.php
+++ commands/Component.php
@@ -9,39 +9,46 @@
 		protected string $signature = 'make:component';
 		protected string $description = 'Creates stream wire component';
 
-		public function handle($className = ''): void
+		public function handle(string $className = ''): void
 		{
-			if (!$className) {
+			if (empty($className)) {
 				$this->error('Component class name is required.');
 				return;
 			}
 
 			$this->info('⏳ Initializing component class file generation...');
 
-			$className = preg_replace('/[^A-Za-z0-9_]/', '', $className);
-			$className = ucfirst($className);
+			$className = preg_replace('/[^A-Za-z0-9_\/]/', '', "component/$className");
+			$directories = explode('/', $className);
+			$className = ucfirst($directories[count($directories) - 1]);
 
-			$filename = $className . '.php';
-			$content = <<<HTML
+			array_pop($directories);
+			$basePath = dirname('./') . "/" . implode('/', $directories) . "/" . $className;
+			$namespaceDeclaration = "namespace ". implode('\\', array_map('ucfirst', $directories)) . ";";
+
+			$content = <<<PHP
 			<?php
-
-				namespace Components;
+			
+				{$namespaceDeclaration}
 				
 				use App\Utilities\Component;
 				
-				class {$className} extends Component {
-				
-					public function render(): string {
-						return view('index');
+				class {$className} extends Component
+				{
+					public function render(): string
+					{
+						// Renders the component's interface located at "$basePath/index.blade.php".
+						// Pass any necessary data inside the compile() method as an associative array.
+						return \$this->compile();
 					}
 				}
-			HTML;
+			PHP;
 
-			if ($this->create($filename, $content, dirname('./'). '/components')) {
-				$this->success("✅ Component class file '{$filename}' has been successfully created and is ready for use.");
-				return;
+			// Create the PHP file
+			if ($this->create("$className.php", $content, $basePath) && $this->create('index.blade.php', '', $basePath)) {
+				$this->success("✅ Component class file '{$className}.php' has been successfully created at '{$basePath}' and is ready for use.");
+			} else {
+				$this->error("❌ Failed to create the file '{$className}.php' at '{$basePath}'.");
 			}
-
-			$this->error("❌ Failed to create the file '{$filename}'.");
 		}
 	}

f06f90f2932e55f5212ddff08fb23c8aea959ce7
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-11-04 12:59:49 +0800

    added utility class

---
+++ commands/Utilities.php
@@ -0,0 +1,46 @@
+<?php
+
+	namespace Commands;
+
+	use App\Console\Command;
+
+	class Utilities extends Command
+	{
+		protected string $signature = 'make:utility';
+		protected string $description = 'Generates a custom utility class.';
+
+		public function handle(string $className = ''): void
+		{
+			if (empty($className)) {
+				$this->error('Utilities class name is required.');
+				return;
+			}
+
+			$this->info('⏳ Initializing utility class file generation...');
+
+			// Extract class info (handles namespace and directories)
+			$classInfo = $this->extractClassInfo($className, 'handler/Utilities');
+			$className = $classInfo['class'];
+			$basePath = base_path($classInfo['directory']);
+			$namespace = $classInfo['namespace'];
+
+			$content = <<<PHP
+			<?php
+
+				namespace {$namespace};
+
+				class {$className}
+				{
+					/**
+					 * Initialize your utility.
+					 */
+					public function __construct()
+					{
+						// TODO: Add initialization logic if needed
+					}
+				}
+			PHP;
+
+			$this->create("$className.php", $content, $basePath);
+		}
+	}
